e85e4c880a5c0905c99fb73ef101b2f8ba021fd0
Lior Samuel <liordon@gmail.com>
2021-09-29 19:37:05 +0300

    dirty commit before I delete all the failed methods

--- alignment_fixer.py
+++ alignment_fixer.py
@@ -1,4 +1,5 @@
 import argparse
+import enum
 import math
 import random as rng
 
@@ -14,6 +15,15 @@ def euclidian_distance(pt1: tuple, pt2: tuple):
 # this function does not makes sense yet. this threshold needs to be calibrated somehow if I am ever to use it.
 def is_match_sensible(match, image_feature_keys, template_feature_keys):
     return euclidian_distance(template_feature_keys[match.queryIdx].pt, image_feature_keys[match.trainIdx].pt) < 30
+
+
+def flatten_pixel_val(pixel):
+    if pixel < 80:
+        return 10
+    elif pixel < 140:
+        return 70
+    else:
+        return min(255, (pixel - 120) ** 2 + 120)
 
 
 def align_template_to_image_using_homography(template, image, maxFeatures=500, keepPercent=0.2, _debug=False):
@@ -61,28 +71,117 @@ def align_template_to_image_using_homography(template, image, maxFeatures=500, k
     return H
 
 
+def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
+    """Return a sharpened version of the image, using an unsharp mask."""
+    blurred = cv2.GaussianBlur(image, kernel_size, sigma)
+    sharpened = float(amount + 1) * image - float(amount) * blurred
+    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+    sharpened = sharpened.round().astype(np.uint8)
+    if threshold > 0:
+        low_contrast_mask = np.absolute(image - blurred) < threshold
+        np.copyto(sharpened, image, where=low_contrast_mask)
+    return sharpened
+
+
 def align_template_to_image_using_affine_transform(template, image):
-    return cv2.estimateRigidTransform(template, image, True)
+    return cv2.estimateRigidTransform(template, image, False)
 
 
-def find_contours(image, threshold, _debug=False):
-    rng.seed(12345)
+def find_contours(image, threshold, dilation_iterations=1, kernel_size=3, _debug=False):
     # Detect edges using Canny
-    # image = cv2.medianBlur(image, 13)
-    image = cv2.GaussianBlur(image, (11, 11), 0)
     canny_output = cv2.Canny(image, threshold, threshold * 2)
+    # canny_output = cv2.dilate(canny_output, np.ones((kernel_size, kernel_size), np.uint8),
+    #     iterations=1)
+    cv2.imshow("canny", canny_output)
+    cv2.waitKey(0)
+
     # Find contours
     _, contours, hierarchy = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    gray_drawing = filter_and_draw_contours(contours, hierarchy, canny_output.shape, area_threshold=5, _debug=_debug)
+    # gray_drawing = cv2.dilate(gray_drawing, np.ones((kernel_size, kernel_size), np.uint8),
+    #     iterations=dilation_iterations)
+    return canny_output
+
+def filter_and_draw_contours(contours, hierarchy, output_shape, area_threshold, _debug=False):
     # Draw contours
-    drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
+    rng.seed(12345)
+    drawing = np.zeros((output_shape[0], output_shape[1], 3), dtype=np.uint8)
     for i in range(len(contours)):
+        if cv2.contourArea(contours[i]) < area_threshold:
+            continue
         color = (rng.randint(0, 256), rng.randint(0, 256), rng.randint(0, 256))
-        cv2.drawContours(drawing, contours, i, color, 2, cv2.LINE_8, hierarchy, 0)
+        cv2.drawContours(drawing, contours, i, color, -1, cv2.LINE_8, hierarchy, 0)
     if _debug:
         cv2.imshow('Contours', drawing)
         cv2.waitKey(0)
     gray_drawing = cv2.cvtColor(drawing, cv2.COLOR_BGR2GRAY)
     return gray_drawing
+
+
+def present_overlayed_images(image1, image2):
+    print("[DEBUG] presenting aligned images...")
+    # resize both the aligned and template images so we can easily
+    # visualize them on our screen
+    image1_for_visualization = imutils.resize(image1, width=700)
+    image2_for_visualization = imutils.resize(image2, width=700)
+    # our visualization will be *overlaying* the
+    # aligned image on the template, that way we can obtain an idea of
+    # how good our image alignment is
+    output = image1_for_visualization.copy()
+    cv2.addWeighted(image2_for_visualization.copy(), 0.5, output, 0.5, 0, output)
+    # show the output image alignment visualization
+    cv2.imshow("Image Alignment Overlay", output)
+    cv2.waitKey(0)
+
+
+class PreProcess(enum.Enum):
+    NOTHING = 'nothing'
+    GAUSSIAN_BLUR = 'GAUSSIAN'
+    MEDIAN_BLUR = 'MEDIAN'
+    BOX_FILTER_BLUR = 'BOX'
+    CONTOUR_STRIP = 'CONTOUR'
+
+
+def prepare_for_matching(image_to_prepare, kernel, method: PreProcess, _debug=False):
+    if method is PreProcess.NOTHING:
+        image = image_to_prepare
+    elif method is PreProcess.GAUSSIAN_BLUR:
+        image = cv2.GaussianBlur(image_to_prepare, (kernel, kernel), 0)
+    elif method is PreProcess.MEDIAN_BLUR:
+        image = cv2.medianBlur(image_to_prepare, kernel)
+    elif method is PreProcess.BOX_FILTER_BLUR:
+        image = cv2.blur(image_to_prepare, (kernel, kernel))
+    elif method is PreProcess.CONTOUR_STRIP:
+        image = cv2.GaussianBlur(image_to_prepare, (kernel, kernel), 0)
+    # image = cv2.erode(image, (kernel_size,kernel_size))
+    image = unsharp_mask(image)
+    image = cv2.threshold(image, 80, 255, cv2.THRESH_TOZERO)[1]
+    return find_contours(image, 100, dilation_iterations=1, _debug=_debug)
+
+
+def flatten_colors(image_to_flatten):
+    (h, w) = image_to_flatten.shape[:2]
+    for i in range(h):
+        for j in range(w):
+            image_to_flatten[i, j] = flatten_pixel_val(image_to_flatten[i, j])
+    image = image_to_flatten
+    # image = unsharp_mask(image)
+    # cv2.GaussianBlur(image, (kernel_size, kernel_size), 5)
+    # image = cv2.medianBlur(image_to_flatten, kernel_size)
+    # image = cv2.threshold(image, 60, 255, cv2.THRESH_TOZERO)[1]
+    # image = cv2.threshold(image, 130, 255, cv2.THRESH_TOZERO)[1]
+    # image = cv2.erode(image, (kernel_size, kernel_size), iterations=2)
+    # image = cv2.dilate(image, (kernel_size, kernel_size), iterations=9)
+
+    cv2.imshow("flat image", image)
+    cv2.waitKey(0)
+    return image
+
+
+def could_be_defect(contour, defect_size_threshold=20):
+    (x, y, w, h) = cv2.boundingRect(contour)
+    return cv2.contourArea(contour) > defect_size_threshold and not ((w / h > 2) or (h / w > 2))
 
 
 if __name__ == "__main__":
@@ -100,6 +199,11 @@ if __name__ == "__main__":
     print("[INFO] loading images...")
     image = cv2.imread(args["image"])
     template = cv2.imread(args["template"])
+    if image is None or template is None:
+        print("[ERROR] image to reference could not be loaded. please check paths.")
+        exit(-1)
+
+    image_name = args["image"].split("/")[-1]
     # align the images
     print("[INFO] aligning images...")
 
@@ -107,47 +211,62 @@ if __name__ == "__main__":
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
-    image_contours = find_contours(image, 100, _debug=show_debug_windows)
-    template_contours = find_contours(template, 100, _debug=show_debug_windows)
+    print(f"image, method, kernel, grossDiff")
+
+    # for preprocessing_method in PreProcess:
+    #     for kernel_size in range(1, 31, 2):
+    preprocessing_method = PreProcess.MEDIAN_BLUR
+    kernel_size = 5
+
+    image_for_alignment = prepare_for_matching(image, kernel_size, preprocessing_method, _debug=show_debug_windows)
+    template_for_alignment = prepare_for_matching(template, kernel_size, preprocessing_method,
+        _debug=show_debug_windows)
 
     (h, w) = image.shape[:2]
     if use_homography:
-        transformation = align_template_to_image_using_homography(template_contours, image_contours,
-            _debug=show_debug_windows)
+        transformation = align_template_to_image_using_homography(template_for_alignment, image_for_alignment,
+        )  # _debug=show_debug_windows)
         aligned_template = cv2.warpPerspective(template, transformation, (w, h))
+        aligned_template_for_alignment = cv2.warpPerspective(template_for_alignment, transformation, (w, h))
     else:
-        transformation = align_template_to_image_using_affine_transform(template_contours, image_contours)
+        transformation = align_template_to_image_using_affine_transform(template_for_alignment,
+            image_for_alignment)
+        if transformation is None:
+            # print(f"{image_name},{preprocessing_method},{kernel_size},0")
+            print("[ERROR] could not find affine transformation")
+            exit(-1)
+            # continue
         aligned_template = cv2.warpAffine(template, transformation, (w, h))
+        aligned_template_for_alignment = cv2.warpAffine(template_for_alignment, transformation, (w, h))
 
     if show_debug_windows:
-        print("[DEBUG] presenting aligned images...")
-        # resize both the aligned and template images so we can easily
-        # visualize them on our screen
-        aligned_template_for_visualization = imutils.resize(aligned_template, width=700)
-        image_for_visualization = imutils.resize(image, width=700)
-
-        # our visualization will be *overlaying* the
-        # aligned image on the template, that way we can obtain an idea of
-        # how good our image alignment is
-        overlay = image_for_visualization.copy()
-        output = aligned_template_for_visualization.copy()
-        cv2.addWeighted(overlay, 0.5, output, 0.5, 0, output)
-        # show the output image alignment visualization
-        cv2.imshow("Image Alignment Overlay", output)
-        cv2.waitKey(0)
+        present_overlayed_images(aligned_template_for_alignment, image_for_alignment)
+        present_overlayed_images(aligned_template, image)
 
     print("[INFO] diffing images...")
-    area_of_interest = cv2.threshold(aligned_template, 1, 255, cv2.THRESH_BINARY)[1]
+    area_of_interest = cv2.threshold(aligned_template, 0, 255, cv2.THRESH_BINARY)[1]
+    area_of_interest = cv2.erode(area_of_interest, np.ones((21, 21), np.uint8), iterations=3)
+    cv2.imshow("mask", area_of_interest)
+    cv2.waitKey(0)
     image_cropped_for_interest = np.where(area_of_interest, image, area_of_interest)
 
-    # compute the absolute difference between the current frame and
-    # first frame
+    # compute the absolute difference between the images
     image_delta = cv2.absdiff(image, aligned_template)
     image_delta = np.where(area_of_interest, image_delta, area_of_interest)
-    possible_defects = cv2.threshold(image_delta, 25, 255, cv2.THRESH_BINARY)[1]
+    possible_defects = cv2.threshold(image_delta, 35, 255, cv2.THRESH_BINARY)[1]
     possible_defects = cv2.bitwise_and(possible_defects, area_of_interest)
+
+    # print(f"{image_name},{preprocessing_method},{kernel_size},{cv2.countNonZero(possible_defects)}")
 
     if show_debug_windows:
         cv2.imshow("Image Difference", image_delta)
         cv2.imshow("Difference Mask", possible_defects)
         cv2.waitKey(0)
+
+    _, contours, hierarchy = cv2.findContours(possible_defects, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = [contour for contour in contours if could_be_defect(contour)]
+    gray_drawing = filter_and_draw_contours(contours, hierarchy, image.shape, area_threshold=0,
+        _debug=show_debug_windows)
+
+    cv2.imshow("Result", gray_drawing)
+    cv2.waitKey(0)
